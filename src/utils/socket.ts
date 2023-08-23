a73273d6767eb411bd46c81df6c957b982553699
Sivert Gullberg Hansen <sivert-gullberg.hansen@norsk-tipping.no>
2023-08-23 11:43:53 +0200

    updated port

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,6 +1,8 @@
 import { io } from 'socket.io-client';
 
+export const serverPort = 3005;
+
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === 'production' ? '' : 'http://localhost:3005';
+const URL = process.env.NODE_ENV === 'production' ? '' : `http://localhost:${serverPort}`;
 
 export const socket = io(URL);
