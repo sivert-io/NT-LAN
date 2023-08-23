54e12ae130597cbcc6c907ff3003e09a98e5b275
Sivert Gullberg Hansen <sivert-gullberg.hansen@norsk-tipping.no>
2023-08-23 11:43:25 +0200

    prettier

--- src/server/index.js
+++ src/server/index.js
@@ -1,37 +1,46 @@
-const { Server } = require("socket.io");
+const { Server } = require('socket.io');
 
 const heldSeats = {};
+const serverPort = 3005;
 
-const io = new Server(3005, {
+const io = new Server(serverPort, {
   cors: {
-    origin: "*",
+    origin: '*',
   },
 });
 
-io.on("connection", (socket) => {
+io.on('connection', (socket) => {
   console.log(`User(${socket.id.substring(0, 4)}) connected!`);
 
   // Emit the updated list of held seats to all users
-  socket.emit("userHoldSeats", heldSeats);
+  socket.emit('userHoldSeats', heldSeats);
 
-  socket.on("disconnect", () => {
+  socket.on('disconnect', () => {
     delete heldSeats[socket.id];
+
+    console.log(`User(${socket.id.substring(0, 4)}) disconnected!`);
   
     // Emit the updated list of held seats to all users
-    socket.broadcast.emit("userHoldSeats", heldSeats);
+    socket.broadcast.emit('userHoldSeats', heldSeats);
   });
 
-  socket.on("HoldingNewSeats", (seats) => {
+  socket.on('HoldingNewSeats', (seats) => {
     console.log(
-      `User(${socket.id.substring(0, 4)}) holding current seats: ${seats}`
+      `User(${socket.id.substring(0, 4)}) is holding: ${seats.length > 0 ? seats : 'no seats'}`
     );
-  
+    
     // Store the held seats for the current user
     heldSeats[socket.id] = seats;
 
     // Emit the list of held seats to all users
-    socket.broadcast.emit("userHoldSeats", heldSeats);
+    socket.broadcast.emit('userHoldSeats', heldSeats);
   });
+
+  socket.on('submitSeats', (seatsToUpdate) => {
+    console.log(`User(${socket.id.substring(0, 4)}) updated: ${Object.keys(seatsToUpdate)}`);
+
+    // Send updated seats to all users now
+  })
 });
 
-console.log("Server running at port 3005");
+console.log('Server running at port', serverPort);
