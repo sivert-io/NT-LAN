0718ce3de8f8002c941decd7f0f1b05cb528c5b6
Sivert Gullberg Hansen <sivert-gullberg.hansen@norsk-tipping.no>
2023-09-19 14:58:54 +0200

    update error box

--- src/components/connectionError/connecitonError.tsx
+++ src/components/connectionError/connecitonError.tsx
@@ -6,7 +6,7 @@ export function ConnecitonError({
   connectionError: string;
 }) {
   return (
-    <div className="fixed z-50 w-screen h-screen bg-[rgba(0,0,0,.65)] flex items-center justify-center">
+    <div className="fixed z-50 top-0 left-0 right-0 bottom-0 bg-[rgba(0,0,0,.65)] flex items-center justify-center">
       <div className="flex-col text-center p-4 flex gap-6 rounded-lg bg-[#FFC4DD] text-[#82052F] font-medium">
         <p>Seating systemet er nede! 🥺</p>
         <b className="text-xs font-bold">Error Message: {connectionError}</b>
