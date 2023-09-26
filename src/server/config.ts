1242e8276150d43e89145977ce471668f9cc898f
Sivert Gullberg Hansen <sivert-gullberg.hansen@norsk-tipping.no>
2023-09-26 11:40:44 +0200

    Fix dates

--- src/server/config.ts
+++ src/server/config.ts
@@ -1,3 +1,3 @@
-export const LAN_DATES = ["2023-09-28", "2023-09-29", "2023-09-30"];
+export const LAN_DATES = ["2023-09-29", "2023-09-30", "2023-10-01"];
 
 export const ADMINS = ['a01799', 'a01794', 'a01760', 'a01513'] as const;
