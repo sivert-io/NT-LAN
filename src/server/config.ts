97ad1749c483723c1f452f8cfda78c53ab12b835
Hansen, Sivert Gullberg <sivert-gullberg.hansen@norsk-tipping.no>
2024-09-24 13:29:10 +0200

    Updated num seats

--- src/server/config.ts
+++ src/server/config.ts
@@ -2,7 +2,7 @@ export const LAN_DATES = ["2024-09-27", "2024-09-28", "2024-09-29"];
 
 export const ADMINS = ["a01799", "a01794", "a01760", "a01513"] as const;
 
-export const totalNumberOfSeats = 98 as const;
+export const totalNumberOfSeats = 84 as const;
 
 export const numberOfColumns = 7 as const;
 export const numberOfColumnsClass = "grid-cols-7" as const;
