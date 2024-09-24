28eb04d58821bf5cd0f8145444992c0f6e772303
Hansen, Sivert Gullberg <sivert-gullberg.hansen@norsk-tipping.no>
2024-09-24 09:58:19 +0200

    Updated dates

--- src/server/config.ts
+++ src/server/config.ts
@@ -1,3 +1,3 @@
-export const LAN_DATES = ["2024-02-16", "2024-02-17", "2024-02-18"];
+export const LAN_DATES = ["2024-09-27", "2024-09-28", "2024-09-29"];
 
-export const ADMINS = ['a01799', 'a01794', 'a01760', 'a01513'] as const;
+export const ADMINS = ["a01799", "a01794", "a01760", "a01513"] as const;
